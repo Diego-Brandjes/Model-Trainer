cdc72c47cfad58e3a3e2c8d6b5686b7c4c43d31d
Diego-Brandjes <diegobrandjes@hotmail.com>
2023-12-22 15:07:27 +0100

    added place in path

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import subprocess
 import requests
+import os
+import sys
 
+
+# WINDOWS INSTALLER
 def check_opencv_installed():
     try:
         # Attempt to import OpenCV
@@ -11,13 +15,25 @@ def check_opencv_installed():
         print("OpenCV is not present or installed correctly.")
         return False
 
+def add_opencv_to_path(opencv_path):
+    try:
+        # Get the current PATH variable
+        current_path = os.environ.get('PATH', '')
+
+        # Add the OpenCV bin directory to the PATH
+        opencv_bin_path = os.path.join(opencv_path, 'build', 'x64', 'vc15', 'bin')
+        new_path = f"{current_path};{opencv_bin_path}"
+
+        # Update the PATH variable
+        os.environ['PATH'] = new_path
+
+        print(f"Added {opencv_bin_path} to the PATH.")
+
+    except Exception as e:
+        print(f"Error adding OpenCV to PATH: {e}")
+
 def install_requirements():
-    try:          
-        # Install the required packages using pip
-        subprocess.check_call(['pip', 'install', 'opencv-python'])
-
-        print("All required packages installed successfully.")
-
+    try:
         if not check_opencv_installed():
             # URL of the OpenCV installer
             opencv_url = "https://sourceforge.net/projects/opencvlibrary/files/3.4.16/opencv-3.4.16-vc14_vc15.exe/download"
@@ -31,9 +47,12 @@ def install_requirements():
             subprocess.run(["opencv_installer.exe"])
 
             print("OpenCV installed successfully.")
+
+            # Add OpenCV to the PATH
+            add_opencv_to_path(os.getcwd())
+
         else:
             print("Skipping OpenCV installation as it is already installed.")
-
 
     except Exception as e:
         print(f"Error: {e}")
